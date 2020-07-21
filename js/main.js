5feccae28eaee97139eb46bd174945315bff8db5
LeoCastaneda <leo.castaneda@osaasllc.com>
2020-07-21 00:47:47 -0500

    more more more

--- js/main.js
+++ js/main.js
@@ -1,2 +1,35 @@
+// let whatsapp_number = '+31621482603';
+// let whatsapp_message = 'Buenas tardes, me gustaría solicitar información';
+// let facebook_id = 'WasteMartMX';
+
+// let btn_whatsapp = `<li class="hide-on-large-only"><a href="https://api.whatsapp.com/send?phone=${whatsapp_number}&text=${whatsapp_message}" target="_blank" class="btn-floating green tooltipped" data-position="left" data-tooltip="Pregúntanos"> <img class="fab-img" src="/img/social-networks/whatsapp-logo.svg"></a></li>
+
+//     <li class="hide-on-med-and-down"><a href="https://web.whatsapp.com/send?phone=${whatsapp_number}&text=${whatsapp_message}n" target="_blank" class="btn-floating green tooltipped" data-position="left" data-tooltip="Pregúntanos"> <img class="fab-img" src="/img/social-networks/whatsapp-logo.svg"></a></li>`;
+
+// let btn_fb = `<li><a href="https://www.facebook.com/${facebook_id}" target="_blank" class="btn-floating blue tooltipped" data-position="left" data-tooltip="Síguenos"> <img class="fab-img" src="/img/social-networks/facebook-logo.png"></a></li>`;
+
+// let btn_messenger = `<li><a href="https://m.me/${facebook_id}/" target="_blank" class="btn-floating blue tooltipped" data-position="left" data-tooltip="Chatea on nosotros"> <img class="fab-img" src="/img/social-networks/messenger.png"></a></li>`;
+
+// let floatingBtn = `<div id="featureHelp" class="tap-target primary no-autoinit" data-target="menu">
+//             <div class="tap-target-content white-text">
+//                 <h5>Contactanos</h5>
+//                 <p>Has click en este boton para ver todas las formas de contactarnos</p>
+//             </div>
+//         </div>
+//         <div id="menu" class="fixed-action-btn">
+//             <a id="social-fab" class="btn-floating btn-large morado">
+//                 <i data-position="left" data-tooltip="Asistencia en línea" class="large material-icons box tooltipped">help_outline</i>
+//             </a>
+//             <ul>
+//                 ${btn_whatsapp}
+//                 ${btn_fb}
+//                 ${btn_messenger}
+//                 <li class="hide-on-med-and-up"><a href="tel:${whatsapp_number}" class="btn-floating green tooltipped" data-position="left" data-tooltip="Llamanos ${whatsapp_number}"> <i class="large material-icons box">phone</i></a></li>
+//                 <li><a href="mailto:contacto@tatenomasquimicos.com" class="btn-floating purple tooltipped" data-position="left" data-tooltip="Email"> <i class="large material-icons box">mail</i></a></li>
+//             </ul>
+//         </div>`;
+
+// $(floatingBtn).appendTo('body');
+
 M.AutoInit();
 console.log('MaterializeCSS auto initaited!');
